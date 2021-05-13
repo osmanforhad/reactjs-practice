5b64354f7fedcc5a2e28f42e2c6454700231c9e7
osmanforhad <osmanforhad93@gmail.com>
2021-05-13 12:41:19 +0600

    understanding the jsx concept

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello React
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React js Practise and Learning
-        </a>
-      </header>
+     <h1>{2+3+5}</h1>
     </div>
   );
 }
