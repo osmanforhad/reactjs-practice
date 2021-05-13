cd9783e5d359e8866026fa307d82ad0fb9daa304
osmanforhad <osmanforhad93@gmail.com>
2021-05-13 17:49:38 +0600

    working with arrow function and passing the argument

--- react-app/my-app/src/App.js
+++ react-app/my-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Welcome from './mycomponent/Welcome';
+import Arrow from './mycomponent/Arrow';
 
 function App() {
   return (
     <div className="App">
       <Welcome></Welcome>
+      <Arrow/>
     </div>
   );
 }
