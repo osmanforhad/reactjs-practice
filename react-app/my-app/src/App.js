8731442c6990a2383338675d1238e75df224f2a9
osmanforhad <osmanforhad93@gmail.com>
2021-05-14 20:47:22 +0600

    working with force update

--- react-app/my-app/src/App.js
+++ react-app/my-app/src/App.js
@@ -1,19 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Welcome from './mycomponent/Welcome';
-import Arrow from './mycomponent/Arrow';
-import Stateexample from './mycomponent/Stateexample';
-import Changestate from './mycomponent/Changestate';
-import Condition1 from './mycomponent/Condition1';
+import Refresh from './mycomponent/Refresh';
 
 function App() {
   return (
     <div className="App">
-      <Welcome></Welcome>
-      <Arrow/>
-      <Stateexample></Stateexample>
-      <Condition1></Condition1>
-      <Changestate></Changestate>
+     
+      <Refresh></Refresh>
       
     </div>
   );
