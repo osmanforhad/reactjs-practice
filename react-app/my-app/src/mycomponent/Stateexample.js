6e590e14781023df32f86b9bcfcbc4dfabe6317a
osmanforhad <osmanforhad93@gmail.com>
2021-05-14 11:41:28 +0600

    nested object in react state

--- react-app/my-app/src/mycomponent/Stateexample.js
+++ react-app/my-app/src/mycomponent/Stateexample.js
@@ -8,18 +8,22 @@ class Stateexample extends Component{
 
         this.state={
             name:"osman gani forhad",
-            age:"27",
+            age:["27","30", "60","70"],
             height:"5 feet 6 inch",
-            weight: "56 kg"
+            weight: {
+                class7: ["27","30", "60","70"],
+                class8: "43",
+                class9: "48"
+            }
         }
     }
 
 render(){
     return <div>
 <h1>My Name is: {this.state.name}</h1>
-<h1>My Age is: {this.state.age}</h1>
+<h1>My Age is: {this.state.age[2]}</h1>
 <h1>My Height is: {this.state.height}</h1>
-<h1>My Weight is: {this.state.weight}</h1>
+<h1>My Weight is: {this.state.weight.class7[1]}</h1>
     </div>
 }
 
