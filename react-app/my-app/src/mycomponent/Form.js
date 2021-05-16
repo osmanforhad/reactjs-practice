cb0948ac2d6419e8231ae852dfcdae94b17c8b8f
osmanforhad <osmanforhad93@gmail.com>
2021-05-16 21:00:02 +0600

    event haldeler using onChange method

--- react-app/my-app/src/mycomponent/Form.js
+++ react-app/my-app/src/mycomponent/Form.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
+
+    constructor() {
+        super()
+        this.state={
+            name: " "
+        }
+    }
+
+    onChangeHandler=(event)=>{
+        var newName=event.target.value;
+        this.setState({name:newName})
+    }
+
     render() {
         return (
             <div>
                 <form>
                     <p>My First Form</p>
-                    <input type="text" placeholder="Your Name"></input>
+                    <p>{this.state.name}</p>
+                    <input onChange={this.onChangeHandler} type="text" placeholder="Your Name"></input>
                     <br></br>
                     <input type="submit" value="Submit Now"></input>
                 </form>
